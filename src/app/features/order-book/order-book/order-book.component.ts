b460b2f88e5a01fc7b1d9faf9fe388dc48767ea9
Mixalloff <m.mikhalev@gateline.ru>
2025-04-08 12:17:53 +0300

    feat: added websocket connection timeout handling

--- src/app/features/order-book/order-book/order-book.component.ts
+++ src/app/features/order-book/order-book/order-book.component.ts
@@ -3,7 +3,7 @@ import { OrderBookData, OrderBookLevel, OrderBookWebSocketData } from '@models/o
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { WebSocketService } from '@services/websocket.service';
 import { LoaderComponent } from '@shared/loader/loader.component';
-import { EMPTY, catchError, tap } from 'rxjs';
+import { EMPTY, Subscription, catchError, finalize, retry, take, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'sprk-order-book',
@@ -24,6 +24,10 @@ export class OrderBookComponent implements OnInit, OnDestroy {
   });
   
   isLoading = signal(true);
+  isError = signal(false);
+
+  private lastMessageTime!: number;
+  private pingTimeoutSubscription!: Subscription;
   
   private readonly webSocketService = inject(WebSocketService);
   private readonly destroyRef = inject(DestroyRef);
@@ -39,18 +43,64 @@ export class OrderBookComponent implements OnInit, OnDestroy {
   onRemove(): void {
     this.remove.emit();
   }
+
+  onReconnect(): void {
+    this.connectToOrderBookStream();
+  }
   
   private connectToOrderBookStream(): void {
-    this.webSocketService.getOrderBookStream(this.symbol())
+    const RETRY_TIMES = 5;
+    const RETRY_DELAY = 2000;
+
+    const stream$ = this.webSocketService.getOrderBookStream(this.symbol());
+    this.isError.set(false);
+    this.isLoading.set(true);
+
+    // Set isLoading to false after the first data is received
+    stream$.pipe(
+      takeUntilDestroyed(this.destroyRef),
+      take(1),
+      tap(() => this.createConnectionTimeoutSubscribe()),
+      tap(() => this.isLoading.set(false)),
+    ).subscribe();
+
+    stream$.pipe(
+      takeUntilDestroyed(this.destroyRef),
+      tap(data => this.updateOrderBook(data)),
+      tap(() => this.lastMessageTime = Date.now()),
+      retry({ count: RETRY_TIMES, delay: RETRY_DELAY }),
+      catchError(error => {
+        console.error('Error connecting to order book stream:', error);
+        this.isError.set(true);
+        return EMPTY;
+      }),
+      finalize(() => this.isLoading.set(false)),
+    ).subscribe();
+  }
+
+  private createConnectionTimeoutSubscribe(): void {
+    const PING_INTERVAL = 2000;
+    const PING_TIMEOUT = 10_000;
+    this.cleanupTimerSubscription();
+    this.lastMessageTime = Date.now();
+
+    this.pingTimeoutSubscription = timer(0, PING_INTERVAL)
       .pipe(
-        tap(data => this.updateOrderBook(data)),
-        catchError(error => {
-          console.error('Error connecting to order book stream:', error);
-          return EMPTY;
-        }),
-        takeUntilDestroyed(this.destroyRef)
+        takeUntilDestroyed(this.destroyRef),
+        tap(() => {
+          const now = Date.now();
+          if (now - this.lastMessageTime > PING_TIMEOUT) {
+            console.warn('No data timeout — possible disconnection');
+            this.isError.set(true);
+            this.cleanupTimerSubscription();
+          }
+        })
       )
       .subscribe();
+  }
+
+  private cleanupTimerSubscription(): void {
+    this.pingTimeoutSubscription?.unsubscribe();
   }
   
   private disconnectFromOrderBookStream(): void {
@@ -58,24 +108,24 @@ export class OrderBookComponent implements OnInit, OnDestroy {
   }
   
   private updateOrderBook(data: OrderBookWebSocketData): void {
-    this.orderBookData.update((currentData: OrderBookData) => {
+    // Reset error state if data is received
+    if (data && this.isError()) {
+      this.isError.set(false);
+    }
+
+    this.orderBookData.update(() => {
       return {
         lastUpdateId: data.lastUpdateId,
         bids: data.bids.map(this.mapOrderLevel),
         asks: data.asks.map(this.mapOrderLevel)
       };
     });
-    
-    // After receiving the first data, set isLoading to false
-    if (this.isLoading()) {
-      this.isLoading.set(false);
-    }
   }
   
   private mapOrderLevel(level: string[]): OrderBookLevel {
     return {
       price: level[0],
-      quantity: level[1]
+      quantity: level[1],
     };
   }
 } 
